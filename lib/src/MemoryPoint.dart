e411f2d1b107d5a37a1c2b0705522ac22deb5cd5
unknown <fridolin@memory-path.com>
2021-03-08 15:23:20 +0100

    Removed MemoryPointId

--- lib/src/MemoryPoint.dart
+++ lib/src/MemoryPoint.dart
@@ -2,23 +2,15 @@ import 'package:latlong/latlong.dart';
 import 'package:memorypath_db_api/memorypath_db_api.dart';
 
 class MemoryPoint {
-  int id;
   String name;
   String image;
   String question;
   String answer;
   LatLng latlng;
 
-  MemoryPoint(
-      {this.id,
-      this.name,
-      this.image,
-      this.question,
-      this.answer,
-      this.latlng});
+  MemoryPoint({this.name, this.image, this.question, this.answer, this.latlng});
 
   MemoryPoint.fromDb(MemoryPointDb memoryPointDb) {
-    //this.id = memoryPointDb.id;
     this.name = memoryPointDb.name;
     this.image = memoryPointDb.image;
     this.answer = memoryPointDb.answer;
@@ -28,7 +20,6 @@ class MemoryPoint {
 
   MemoryPointDb toMemoryPointDb() {
     return MemoryPointDb(
-        //id: this.id,
         name: this.name,
         question: this.question,
         image: this.image,
