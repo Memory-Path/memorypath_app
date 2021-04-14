1ad96926466ee125f39f5f0ffeda8dbf72aa3e0c
unknown <fridolin@memory-path.com>
2021-04-14 11:09:51 +0200

    minor adaption of EditMemoryPointWidget for user-workflow

--- lib/widgets/EditMemoryPointWidget.dart
+++ lib/widgets/EditMemoryPointWidget.dart
@@ -22,16 +22,12 @@ class _EditMemoryPointWidgetState extends State<EditMemoryPointWidget> {
   TextEditingController _questionController;
   TextEditingController _answerController;
 
-  /// MemoryPoint-State to track the actual values that the User inserted/edited
-  MemoryPointDb _memoryPointDbState;
-
   @override
   void initState() {
     ///Initial setting of the values for the Widget-State
-    _memoryPointDbState = widget.memoryPoint;
     _questionController =
-        TextEditingController(text: _memoryPointDbState.question);
-    _answerController = TextEditingController(text: _memoryPointDbState.answer);
+        TextEditingController(text: widget.memoryPoint.question);
+    _answerController = TextEditingController(text: widget.memoryPoint.answer);
     super.initState();
   }
 
@@ -45,12 +41,12 @@ class _EditMemoryPointWidgetState extends State<EditMemoryPointWidget> {
         shrinkWrap: true,
         children: [
           ///TextField to edit the Title of the MemoryPoint
-          TitleTextField(_onMemoryPointNameChanged, _memoryPointDbState.name),
+          TitleTextField(_onMemoryPointNameChanged, widget.memoryPoint.name),
           ListTile(
               title: Container(
             constraints: BoxConstraints(minHeight: 128),
             child: ImagePickerWidget(
-              imagePath: _memoryPointDbState.image,
+              imagePath: widget.memoryPoint.image,
               onImageChanged: updateImage,
             ),
           )),
@@ -61,7 +57,7 @@ class _EditMemoryPointWidgetState extends State<EditMemoryPointWidget> {
                 labelText: 'Question, Keyword or Note'),
             onSubmitted: (String question) {
               setState(() {
-                _memoryPointDbState.question = question;
+                widget.memoryPoint.question = question;
               });
             },
           ),
@@ -75,40 +71,10 @@ class _EditMemoryPointWidgetState extends State<EditMemoryPointWidget> {
                 border: OutlineInputBorder(), labelText: 'Answer'),
             onSubmitted: (String answer) {
               setState(() {
-                _memoryPointDbState.answer = answer;
+                widget.memoryPoint.answer = answer;
               });
             },
           ),
-
-          /// Button Row for either Deletion or Accepting the MemoryPoint
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              TextButton.icon(
-                onPressed: () {
-                  _onMemoryPointDelete();
-                  Navigator.of(context).pop();
-                },
-                icon: Icon(Icons.delete),
-                label: Text("Delete"),
-              ),
-              TextButton.icon(
-                icon: Icon(Icons.check),
-                label: Text("Accept"),
-                onPressed: () {
-                  if (_memoryPointIsValid()) {
-                    _onMemoryPointUpdate(_memoryPointDbState);
-                    Navigator.of(context).pop();
-                  }
-
-                  /// TODO: SnackBarStyle
-                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      content: Text(
-                          "Memory-Point not valid!\n You have to set all Parameters before submitting.")));
-                },
-              ),
-            ],
-          )
         ],
       ),
     );
@@ -124,39 +90,11 @@ class _EditMemoryPointWidgetState extends State<EditMemoryPointWidget> {
 
   ///Callback for the [ImagePickerWidget] to track the new Path of the Image, selected from the User
   void updateImage(String image) {
-    _memoryPointDbState.image = image;
-  }
-
-  ///Logic check, if all fields are set properly
-  bool _memoryPointIsValid() {
-    if (_memoryPointDbState.name != null &&
-        _memoryPointDbState.name.isNotEmpty &&
-        _memoryPointDbState.image != null &&
-        _memoryPointDbState.answer != null &&
-        _memoryPointDbState.answer.isNotEmpty &&
-        _memoryPointDbState.question != null &&
-        _memoryPointDbState.question.isNotEmpty) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  ///Callback, when a MemoryPoint is deleted
-  void _onMemoryPointDelete() {
-    widget.memoryPoint.delete();
-  }
-
-  ///Callback, when a MemoryPoint is updated
-  void _onMemoryPointUpdate(MemoryPointDb memoryPointDbState) {
-    widget.memoryPoint.name = _memoryPointDbState.name;
-    widget.memoryPoint.image = _memoryPointDbState.image;
-    widget.memoryPoint.question = _memoryPointDbState.question;
-    widget.memoryPoint.answer = _memoryPointDbState.answer;
+    widget.memoryPoint.image = image;
   }
 
   /// Callback for the [TitleTExtField]
   void _onMemoryPointNameChanged(String title) {
-    _memoryPointDbState.name = title;
+    widget.memoryPoint.name = title;
   }
 }
